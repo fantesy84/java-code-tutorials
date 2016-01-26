22f6f7635ba754ff8e25a48f551896425d939fdd
Andronicus Cook <fantesy84@gmail.com>
2016-01-26 18:30:39 +0800

    问题4

---
+++ java-code-tutorials-multi-thread/multithread-question4/src/main/java/net/fantesy84/queue/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 项目名: multithread-question4
+ * 包名:  net.fantesy84.queue
+ * 文件名: package-info.java
+ * Copy Right © 2016 Andronicus Ge
+ * 时间: 2016年1月26日
+ */
+/**
+ * @author Andronicus
+ * @since 2016年1月26日
+ */
+package net.fantesy84.queue;
